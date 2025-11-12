a303f95fa968645dc950b23bfec11ea12e166326
musab <malqarninet@outlook.com>
2025-11-12 13:16:44 +0300

    Debugging Spring Boot Applications using breakpoint and debug button Automatic restart add dependecy for development environment only

--- src/main/java/com/musab/spring_fundamental_course/HomeController.java
+++ src/main/java/com/musab/spring_fundamental_course/HomeController.java
@@ -1,13 +1,19 @@
 package com.musab.spring_fundamental_course;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
+        String viewName = getViewName();
+        // System.out.println(viewName);
+        return viewName;
+    }
+
+    private String getViewName() {
         return "index.html";
     }
 }
