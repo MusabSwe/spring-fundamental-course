140c2d537faee60b95cf92f889015ef8e4d44106
musab <malqarninet@outlook.com>
2025-11-12 11:18:13 +0300

    Mosh spring boot fundamental course set up

---
+++ src/main/java/com/musab/spring_fundamental_course/SpringFundamentalCourseApplication.java
@@ -0,0 +1,14 @@
+package com.musab.spring_fundamental_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringFundamentalCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFundamentalCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musab/spring_fundamental_course/SpringFundamentalCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musab.spring_fundamental_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFundamentalCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
