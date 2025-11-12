f9e806e1249fcbf5b0a5b976ac3659b566b42ef7
musab <malqarninet@outlook.com>
2025-11-12 12:17:52 +0300

    Building Your First Spring Controller add html file for the home path index.html

---
+++ src/main/java/com/musab/spring_fundamental_course/HomeController.java
@@ -0,0 +1,13 @@
+package com.musab.spring_fundamental_course;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
